f5a4a37f7417a50453e5a69c7eb153799bcd67bf
Emiliano Gusberti <emggw@hotmail.com>
2022-12-25 15:12:00 -0300

    Add body parser + html 'back' button

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ require("dotenv").config();
 
 const https = require("https");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true })); // Required to parse requests
 
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
   const url = "https://api.openweathermap.org/data/2.5/weather";
   const apiKey = process.env.WEATHER_APPID;
-  const query = "resistencia";
+  const query = req.body.cityName;
   const units = "metric";
 
-  const fullUrl = `${url}?appid=${apiKey}&q=${query}&units=${units}`;  
+  const fullUrl = `${url}?appid=${apiKey}&q=${query}&units=${units}`;
 
   https.get(fullUrl, function (response) {
     response.on("data", function (data) {
@@ -24,17 +30,21 @@ app.get("/", function (req, res) {
       res.write("<h1>Weather API App</h1>");
       res.write(`<p>Request made at: ${fullUrl}</p>`);
       res.write(`<p>Status code: ${res.statusCode}</p>`);
-      res.write(`<p>The temperature in ${weatherData.name} is: ${temp} &deg;C</p>`);
+      res.write(
+        `<p>The temperature in ${weatherData.name} is: ${temp} &deg;C</p>`
+      );
       res.write(`<p>The weather is currently '${desc}'</p>`);
       res.write(`<img src="${imgURL}" alt="weather-icon">`);
       res.write(`<p>Complete data:</p><ul>`);
-      
+
       for (const [k, v] of Object.entries(weatherData)) {
         res.write(`<li>${k}: ${JSON.stringify(v)}</li>`);
-
       }
 
       res.write(`</ul>`);
+      res.write(
+        `<form action="/" method="get"><button type="submit">Back</button></form>`
+      );
       res.send();
     });
   });
